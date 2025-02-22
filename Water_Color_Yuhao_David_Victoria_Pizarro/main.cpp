fe5158a84a5694ad8c22335623687c803c8bea0c
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2025-02-22 19:08:06 +0100

    creo que solucioné el score xd

--- Water_Color_Yuhao_David_Victoria_Pizarro/main.cpp
+++ Water_Color_Yuhao_David_Victoria_Pizarro/main.cpp
@@ -52,6 +52,7 @@ void tableroInicio(char table[ROWS][COLUMNS]) {
 	table[2][5] = 'S';
 	table[3][21] = 'X';
 	table[3][17] = 'O';
+	table[2][17] = 'X';
 }
 
 void imprimirTablero(char table[ROWS][COLUMNS]) {
@@ -63,6 +64,85 @@ void imprimirTablero(char table[ROWS][COLUMNS]) {
 	}
 }
 
+void puntuacio(char table[ROWS][COLUMNS], int &score, int &turnos) {
+	int columna = 0;
+	int vacio = 0;
+	int canIgual = 0;
+	int dif = 0;
+	bool completa = false;
+	bool voids = false;
+	int win = 0;
+	char liq;
+
+	for (int eleccion = 1; eleccion <= 6; eleccion++) {
+		if (eleccion == 1) { columna = 1; }
+		else if (eleccion == 2) { columna = 5; }
+		else if (eleccion == 3) { columna = 9; }
+		else if (eleccion == 4) { columna = 13; }
+		else if (eleccion == 5) { columna = 17; }
+		else if (eleccion == 6) { columna = 21; }
+
+		for (int a = 1; a < ROWS - 2; a++) {
+			if (table[a][columna] != ' ') {
+				liq = table[a][columna];
+				break;
+			}
+			else if (table[a][columna] == ' ') {
+				vacio++;
+				liq = table[a][columna];
+			}
+		}
+		if (vacio == 3) { voids = true; completa = true; }
+		if (!voids) {
+			for (int a = 1; a < ROWS - 2; a++) {
+				if (table[a][columna] == liq) {
+					canIgual++;
+				}
+			}
+		}
+		if (canIgual == 3) { completa = true; }
+		if (!completa) {
+			for (int a = 1; a < ROWS - 2; a++) {
+				if (table[a][columna] != ' ') {
+					liq = table[a][columna];  // Asignamos el primer líquido encontrado
+					break;
+				}
+			}
+			for (int a = 1; a < ROWS - 2; a++) {
+				if (table[a][columna] != ' ' && table[a][columna] != liq) {
+					dif++;  // Solo cuenta si es un líquido diferente, evitando contar espacios vacíos
+				}
+			}
+		}
+		if (vacio == 3) {
+			win++;
+			score += 50;			
+		}
+		else if (canIgual == 3) {
+			win++;
+			score += 30;
+		}
+		else if (dif < 1) {
+			win++;
+			score += 30;
+		}
+		vacio = 0;
+		canIgual = 0;
+		voids = false;
+		completa = false;
+	}
+	if (win != 6 && turnos != 0) {
+		score = 0;
+	}
+	if (win == 6) {
+		int turnPoints = 3;
+		int cantidad = 3 * turnos;
+		score += cantidad;
+		turnos = 0;
+	}
+	std::cout << win << "\n";
+	win = 0;
+}
 
 void main() {
 
@@ -71,6 +151,8 @@ void main() {
 	int eleccion2 = 0;
 	int turnos = 10;
 	bool completado = false;
+	int score = 0;
+	std::string name;
 	tableroInicio(table);
 	while (turnos > 0) {
 		completado = false;
@@ -79,6 +161,9 @@ void main() {
 		elegirLlenar(eleccion2, eleccion, table);
 		liquido(eleccion, eleccion2, table, completado);
 		if (completado)turnos--;
+		puntuacio(table, score, turnos);
 		std::cout << "Turnos restantes: " << turnos << "\n";
 	}
+	std::cout << "You won!!! Your score is: " << score << "----> Enter your name: ";
+	std::cin >> name;
 }
