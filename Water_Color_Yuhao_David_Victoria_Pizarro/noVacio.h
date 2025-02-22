cd79e9daf0be7c575ce51cf95458ef344b72b957
Victoria Pizarro Moreno <victoria.pizarro@enti.cat>
2025-02-22 02:02:15 +0100

    subir actividad lel

---
+++ Water_Color_Yuhao_David_Victoria_Pizarro/noVacio.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#define ROWS 6
+#define COLUMNS 23
+void noVacio(int& eleccion, char table[ROWS][COLUMNS]);
+void comprobarLlena(int& eleccion2, int& eleccion, char table[ROWS][COLUMNS]);
